db2d6134bfbe56524bafd676cc6cf3873729420b
SimpleA <andy6040164@gmail.com>
2016-09-23 13:29:48 +0800

    third commit

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -1,21 +1,22 @@
-#!/bin/bash
-
 import sys
 
 if len(sys.argv) != 3 :
     raise Exception('There should be two arguments. Format ./Q1.sh [column] [file]')
-
-col = int(sys.argv[1])
+try :
+    col = int(sys.argv[1])
+except ValueError :
+    raise Exception('column index should be an integer')
 fileName = sys.argv[2]
 ret = []
 ret_str = []
 with open(fileName) as f :
     for line in f :
         line = line.strip(' ')
+        line = line.strip('\n')
         fields = line.split(' ')
         # check that the specified column exists
-        if col >= len(fields) :
-            errMsg = 'column ' + str(col) + ' does not exist in ' + line
+        if col >= len(fields) or col < 0:
+            errMsg = 'column ' + str(col) + ' exceeds boundary ' 
             raise Exception(errMsg)
         ret.append([float(fields[col]), fields[col]])
 
@@ -23,4 +24,3 @@ ret = sorted(ret, key=lambda k:k[0])
 
 with open('ans1.txt', 'w') as outFile :
     outFile.write(','.join(s[1] for s in ret) + '\n')
-
--- hw0/Q2.py
+++ hw0/Q2.py
@@ -1,10 +1,9 @@
-#!/bin/bash
 
 from PIL import Image
 import sys
 
 if len(sys.argv) != 2 :
-    raise Exception('There shoud be exactly one argument')
+    raise Exception('Usage: ./Q2.sh file')
 
 img = Image.open(str(sys.argv[1]))
 img = img.rotate(180)
